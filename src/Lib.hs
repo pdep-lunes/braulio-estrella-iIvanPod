2d99033bbd24a1519c73172c4658a4b47e6b1ad6
Ivan Preinsperger <ipreinsperger@frba.utn.edu.ar>
2025-04-28 12:20:39 -0300

    Primeros cambios del ejercicio

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,5 +2,21 @@ module Lib () where
 
 import Text.Show.Functions ()
 
-doble :: Int -> Int
-doble x = x * 2
+type Nombre = String
+type SuperPoderActivo = Bool
+type PoderBasico = String
+type SuperPoder = String
+type CantidadDeVida = Int
+
+type Personaje = (Nombre , PoderBasico, SuperPoder, SuperPoderActivo ,CantidadDeVida)
+
+bolaEspinosa :: Personaje -> Int
+bolaEspinosa unPersonaje = (cantidadDeVida unPersonaje) - 1000 
+
+cantidadDeVida :: Personaje -> Int
+cantidadDeVida ( _ , _ , _ , _ , vida) = vida
+
+
+
+espina :: Personaje
+espina = ("Espina" , "Bola de Espinas" , "Grana de espinas", True , 4800)
